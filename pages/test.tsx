e5ed806d60a94a6e65fcebad81ecc953f1694b58
unknown <ylyangliu1@gmail.com>
2021-08-23 16:08:02 -0700

    work in progress

--- pages/test.tsx
+++ pages/test.tsx
@@ -6,20 +6,37 @@ import Image from "next/image";
 import Link from "next/link";
 import { Container, Row, Col } from "react-bootstrap";
 import "animate.css";
+import styled from "styled-components";
 
 const Test: NextPage = () => {
   return (
     <div>
-      <h1 style={{ flex: 1, textAlign: "center" }}>Testing Page</h1>
-      <Image
-        src="https://source.unsplash.com/random/400x200"
-        alt="Pokeball"
-        width={400}
-        height={200}
-        layout="fixed"
-      />
+      <TestWrapper>
+        <h1>Testing Page</h1>
+        <Image
+          src="https://source.unsplash.com/random/400x200"
+          alt="Pokeball"
+          width={400}
+          height={200}
+          // layout="fixed"
+        />
+      </TestWrapper>
+      <TabWrapper>
+        <p>hello</p>
+      </TabWrapper>
     </div>
   );
 };
+
+const TestWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TabWrapper = styled.div`
+  border-radius: 20px;
+  padding: 40px;
+`;
 
 export default Test;
